9633ac85767fbd87dc3af9a9f0c63f5a3f8d3e3a
Tharak-N <114059542+Tharak-N@users.noreply.github.com>
2023-08-24 17:35:26 +0530

    adding useFactory example

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ import { employeeName } from './full-name';
  * @param TOKEN - EMPLOYEE string
  * @param PROVIDER - we can register either string / object with this useValue
  */
-container.register('EMPLOYEE', { useValue: employeeName });
+// container.register('EMPLOYEE', { useValue: employeeName });
 // .resolve('EMPLOYEE')
 // .logResolvedInstance();
 
@@ -25,12 +25,31 @@ import { InjectDecorator } from './inject-decorator';
  * @param TOKEN - INJECT_DECORATOR string
  * @param PROVIDER - class
  */
-container
-  .register('INJECT_DECORATOR', { useClass: InjectDecorator })
-  .resolve('INJECT_DECORATOR')
-  .logResolvedInstance();
+// container
+//   .register('INJECT_DECORATOR', { useClass: InjectDecorator })
+//   .resolve('INJECT_DECORATOR')
+//   .logResolvedInstance();
 
 import { AutoInjectable } from './auto-injectable';
-container.resolve(AutoInjectable).logResolvedInstance();
+import { DependencyContainer } from 'tsyringe';
+container.register('FACT', {
+  useFactory: (dependency: DependencyContainer) => {
+    console.log('dep', dependency);
+    let isRegistered = dependency.isRegistered('EMPLOYEE');
+    console.log('uis registeres', isRegistered);
+    if (isRegistered) {
+      console.log('inside if');
+      container.resolve('EMPLOYEE').logResolvedInstance();
+    } else {
+      console.log('inside else');
+      dependency.register('EMPLOYEE', { useValue: employeeName });
+      // container.resolve('EMPLOYEE').logResolvedInstance()
+    }
+  },
+});
+
+container.resolve('FACT');
+console.log('after FACT resolving');
+container.resolve('EMPLOYEE').logResolvedInstance();
 
 // let autoInjectTester = new AutoInjectableTester().getAutoInjectInstance()
